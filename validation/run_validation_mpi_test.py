f32feb2ab589e79b95fed3be03d7581179260f94
Francesco Casalegno <francesco.casalegno@gmail.com>
2017-05-05 12:23:32 +0200

    run mpi tests

---
+++ validation/run_validation_mpi_test.py
@@ -0,0 +1,17 @@
+import nose
+import os
+import sys
+
+if __name__ == '__main__':
+    # It's expected that we are being run from the test subdirectory,
+    # and so all the test modules and the mesh directory is under
+    # 'validation/'.
+
+    nose.run(argv=['-s', '-v', 'validation_rd_mpi/unbdiff2D.py'])
+    nose.run(argv=['-s', '-v', 'validation_rd_mpi/unbdiff2D_linesource_ring.py'])
+    nose.run(argv=['-s', '-v', 'validation_rd_mpi/unbdiff.py'])
+    nose.run(argv=['-s', '-v', 'validation_rd_mpi/bounddiff.py'])
+    nose.run(argv=['-s', '-v', 'validation_rd_mpi/csd_clamp.py'])
+    nose.run(argv=['-s', '-v', 'validation_rd_mpi/masteq_diff.py'])
+    nose.run(argv=['-s', '-v', 'validation_rd_mpi/kisilevich.py'])
+    nose.run(argv=['-s', '-v', 'validation_efield_mpi/test_rallpack1_dist.py'])
