5e1ddda11856804f4d63869fb6dbe312dc0b43f2
Alessandro Cattabiani <alessandro.cattabiani@epfl.ch>
2022-01-11 11:13:00 +0100

    add test_master_20220110: after GHK temperature fix

--- postproc/caburst.py
+++ postproc/caburst.py
@@ -39,7 +39,8 @@ traces_b.append(Trace("t", "s", multi=multi))
 benchmark_STEPS3 = TraceDB(
     "STEPS3",
     traces_b,
-    "caburst/benchmark_STEPS3/results/test_master_20211216",
+    "caburst/benchmark_STEPS3/results/test_master_20220110",
+    # "caburst/benchmark_STEPS3/results/test_master_20211216",
     # "caburst/benchmark_STEPS3/results/master_metis_split_1024tasks_20211212",
     clear_raw_traces_cache=True,
     clear_refined_traces_cache=True,
@@ -76,7 +77,8 @@ traces_s.append(Trace("t", "s", multi=multi))
 sample_STEPS4 = TraceDB(
     "STEPS4",
     traces_s,
-    "caburst/sample_STEPS4/results/test_master_20211216",
+    "caburst/sample_STEPS4/results/test_master_20220110",
+    # "caburst/sample_STEPS4/results/test_master_20211216",
     # "caburst/sample_STEPS4/results",
     # "caburst/benchmark_STEPS3/results/test",
     # "caburst/sample_STEPS4/results/master_split_1024tasks_20211212",
