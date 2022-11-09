f2c4e877a2a4fcf7c4d99251e875e528cf6221cf
Iain Hepburn <ihepburn@oist.jp>
2022-11-09 10:24:34 +0900

    Add sim.setTriOhmicErev to tetopsplit rallpack1 test for the leak channels (recovered from 0mV as default).

---
+++ validation/run_validation_efield_mpi_tests.py
@@ -0,0 +1,24 @@
+import contextlib
+import os.path as osp
+import sys
+import unittest
+
+if __name__ == '__main__':
+    test_dir = osp.dirname(osp.abspath(__file__))
+    top_dir = osp.join(test_dir, '..')
+    loader = unittest.TestLoader()
+
+    directories = []
+    for fold in sys.argv[1:]:
+        directories.append(osp.join(test_dir, fold))
+    if len(directories) == 0:
+        directories.append(test_dir)
+
+    for start_dir in directories:
+        print(f'Running validations from {start_dir}')
+
+        with contextlib.redirect_stdout(None):
+            suite = loader.discover(start_dir, pattern='parallel_rallpack*.py', top_level_dir=top_dir)
+
+        runner = unittest.TextTestRunner(verbosity=2)
+        runner.run(suite)
