c57755a9a09ac0864a7001e8482670bfb4c77f96
Iain Hepburn <ihepburn@oist.jp>
2024-03-13 13:41:07 +0900

    Add individual data points to plot vesiclediff_rmsd.pdf (figure 8a). Refactor to save and load data.

--- vesicles/theory/vesdiff_rmsd.py
+++ vesicles/theory/vesdiff_rmsd.py
@@ -1,13 +1,13 @@
-from matplotlib import pyplot as plt
 import numpy as np
 from math import *
 
+# the number of samples of the random steps
+n = 1000000
 
-root_mean_squared_disps=np.arange(0.02, 2.02, 0.02) #relative to diameter
+root_mean_squared_disps=np.arange(0.02, 2.02, 0.04) #relative to diameter
 proportion = []
 for rmsd in root_mean_squared_disps:
     print ("RMSD =", rmsd, "diameters")
-    n = 1000000
     dist=np.zeros(n)
     for i in range(n):
         dx = np.random.normal()*rmsd/sqrt(3)
@@ -20,11 +20,7 @@ for rmsd in root_mean_squared_disps:
     
     proportion.append(total_above_2diam/n)
 
-
-plt.plot(root_mean_squared_disps, np.array(proportion)*100)
-fig = plt.gcf()
-fig.set_size_inches(3.4, 3.4)
-plt.xlabel("RMSD (/diameter)")
-plt.ylabel("Diffusion steps over 2 diameters (%)")
-plt.savefig('plots/vesiclediff_rmsd.pdf', dpi=300, bbox_inches='tight')
+with open('data/rmsd_'+str(n)+'.npy', 'wb') as f:
+    np.save(f, root_mean_squared_disps)
+    np.save(f, proportion)
 
---
+++ vesicles/theory/vesdiff_rmsd_plot.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+# the number of samples of the random steps
+n = 1000000
+
+with open('data/rmsd_'+str(n)+'.npy', 'rb') as f:
+    root_mean_squared_disps = np.load(f)
+    proportion = np.load(f)
+
+plt.plot(root_mean_squared_disps, np.array(proportion)*100, marker='o', ms=3)
+fig = plt.gcf()
+fig.set_size_inches(3.4, 3.4)
+plt.xlabel("RMSD (/diameter)")
+plt.ylabel("Diffusion steps over 2 diameters (%)")
+plt.savefig('plots/vesiclediff_rmsd.pdf', dpi=300, bbox_inches='tight')
+
