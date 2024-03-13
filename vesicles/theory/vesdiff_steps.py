5480aa8e8798c315db1a45e2758332baeeeeecc0
Iain Hepburn <ihepburn@oist.jp>
2024-03-13 13:56:26 +0900

    Allow data saving and loading for vesdiff_steps model.

--- vesicles/theory/vesdiff_steps.py
+++ vesicles/theory/vesdiff_steps.py
@@ -1,30 +1,25 @@
-from matplotlib import pyplot as plt
 import numpy as np
 from random import *
 from math import *
 
 
 D=0.06e-12
+dts = [1e-4, 1e-3]
 
-for t in [1e-4, 1e-3]:
-    n = 5000000
+# the number of samples of the random steps
+n = 5000000
+
+f = open('data/steps_'+str(n)+'.npy', 'wb')
+np.save(f, dts)
+
+for dt in dts:
     dist=np.zeros(n)
     
     for i in range(n):
-
-        dx = np.random.normal()*sqrt(2*D*t) 
-        dy = np.random.normal()*sqrt(2*D*t)
-        dz = np.random.normal()*sqrt(2*D*t)
+        dx = np.random.normal()*sqrt(2*D*dt)
+        dy = np.random.normal()*sqrt(2*D*dt)
+        dz = np.random.normal()*sqrt(2*D*dt)
 
         dist[i] = sqrt(dx**2+dy**2+dz**2)*1e9
     
-    plt.hist(dist, bins=np.arange(0, 50, 0.5), density=True, label='${\Delta t}$='+str(t*1e3)+' ms', alpha=0.7)
-
-fig = plt.gcf()
-fig.set_size_inches(3.4, 3.4)
-
-plt.xlabel("Vesicle diffusion distance (nm)")
-plt.ylabel("Probability density")
-plt.legend()
-plt.savefig('plots/vesiclediff_steps.pdf', dpi=300, bbox_inches='tight')
-
+    np.save(f, dist)
---
+++ vesicles/theory/vesdiff_steps_plot.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from random import *
+from math import *
+
+# the number of samples of the random steps
+n = 5000000
+
+f = open('data/steps_'+str(n)+'.npy', 'rb')
+dts = np.load(f)
+
+for dt in dts:
+    dist = np.load(f)
+    plt.hist(dist, bins=np.arange(0, 50, 0.5), density=True, label='${\Delta t}$='+str(dt*1e3)+' ms', alpha=0.7)
+
+fig = plt.gcf()
+fig.set_size_inches(3.4, 3.4)
+
+plt.xlabel("Vesicle diffusion distance (nm)")
+plt.ylabel("Probability density")
+plt.legend()
+plt.savefig('plots/vesiclediff_steps.pdf', dpi=300, bbox_inches='tight')
+
